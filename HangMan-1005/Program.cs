0685fd073310ba7eaa369088c5024e8a4e010c39
Gusts Linkevics <gusts@gst.lv>
2021-06-28 18:36:18 +0300

    Add project files.

---
+++ HangMan-1005/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HangMan_1005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
